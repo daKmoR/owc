32fc5de5496404f36872387ee5f7101d0af77b9a
Thomas Allmer <d4kmor@gmail.com>
2019-02-15 03:20:28 +0100

    chore: ok build it

--- src/owc-app.js
+++ src/owc-app.js
@@ -1,19 +1,10 @@
 import { LitElement, html, css } from 'lit-element';
+import { openWc } from './open-wc-logo';
 
 class OwcApp extends LitElement {
-  static get styles() {
-    return css`
-      h1, form {
-        text-align: center;
-      }
-    `;
-  }
-
   static get properties() {
     return {
-      data: {
-        type: Array,
-      }
+      data: { type: Array },
     };
   }
 
@@ -26,6 +17,73 @@ class OwcApp extends LitElement {
     this.data = [];
   }
 
+  static get styles() {
+    return [
+      css`
+        :host {
+          text-align: center;
+          min-height: 100vh;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          font-size: calc(10px + 2vmin);
+          color: #1a2b42;
+        }
+
+        header {
+          margin: auto;
+        }
+
+        svg {
+          animation: app-logo-spin infinite 20s linear;
+        }
+
+        a {
+          color: #217ff9;
+        }
+
+        .app-footer {
+          color: #a8a8a8;
+          font-size: calc(10px + 0.5vmin);
+        }
+
+        @keyframes app-logo-spin {
+          from {
+            transform: rotate(0deg);
+          }
+          to {
+            transform: rotate(360deg);
+          }
+        }
+      `,
+    ];
+  }
+
+  render() {
+    return html`
+      <header class="app-header">
+        ${openWc}
+        <h1>owc</h1>
+      </header>
+      <main>
+        <form>
+          <input type="text" />
+          <button @click=${this._search}>search</button>
+        </form>
+        <ul>
+          ${this.data.map(result => html`
+            <li>${result.package.name}</li>
+          `)}
+        </ul>
+      </main>
+      <p class="app-footer">
+        🚽 Made with love by
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/open-wc">open-wc</a>.
+      </p>
+    `;
+  }
+
   _search(ev) {
     ev.preventDefault();
     this.search(this.searchElement.value);
@@ -34,25 +92,10 @@ class OwcApp extends LitElement {
   async search(query) {
     const url = `/.netlify/functions/search?q=${query}`;
     const response = await fetch(url);
-    let json = await response.json();
+    const json = await response.json();
     console.log(json);
     this.data = [];
     this.data = json.results;
-  }
-
-  render() {
-    return html`
-      <h1>OWC</h1>
-      <form>
-        <input type="text" />
-        <button @click=${this._search}>search</button>
-      </form>
-      <ul>
-        ${this.data.map(result => html`
-          <li>${result.package.name}</li>
-        `)}
-      </ul>
-    `;
   }
 }
 
